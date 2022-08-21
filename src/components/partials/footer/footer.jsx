7535ce597297ae9734bb1d3b591aec5917a310c6
svarnyk <svarnyk@gmail.com>
2022-08-21 13:25:10 +0300

    footer done

--- src/components/partials/footer/footer.jsx
+++ src/components/partials/footer/footer.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import "./footer.css";
 import Heading from "../../primitives/heading/heading";
-import Image from "../../primitives/image";
+import Logo from "../../primitives/logo/logo";
+import { FaTelegramPlane } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="footer_wrapper">
-        <div className="footer_logo">
-          <Image src="../../../assets/logo/Logo_monochrome.png" alt="logo" fit={"center"}/>
+      <div className="footer__wrapper">
+        <div className="footer__logo">
+          <Logo color={"white"}/>
         </div>
-        <div className="footer_about">
+        <div className="footer__about">
           <Heading weight={4}>All Rights Reserved 2022</Heading>
         </div>
-        <div className="footer_social">
-
+        <div className="footer__social">
+          <a className="social__telegram" href="https://web.telegram.org"><FaTelegramPlane color={"white"} size={"33px"}/></a>
+          <a className="social__discord" href="https://discord.com"><FaDiscord color={"white"} size={"33px"}/></a>
+          <a className="social__twitter" href="https://twitter.com"><FaTwitter color={"white"} size={"33px"}/></a>
         </div>
       </div>
     </div>
