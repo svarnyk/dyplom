ea8022bbdde8aea31a7392875cc7b638611309b1
svarnyk <svarnyk@gmail.com>
2022-08-31 18:41:16 +0300

    finished creating a VideoGallery

--- src/components/partials/videoGallery/videoGallery.jsx
+++ src/components/partials/videoGallery/videoGallery.jsx
@@ -2,24 +2,57 @@ import React from "react";
 import VideoCard from "../videoCard/videoCard";
 import Button from "../../primitives/button/button";
 import Heading from "../../primitives/heading/heading";
+import { FaYoutube } from "react-icons/fa";
 import "./videoGallery.css";
+import PropTypes from "prop-types";
 
 export default function VideoGallery(props) {
+  const content = props.content;
+  const result = content.map((video) =>
+    <VideoCard
+      urlVideo={video.url}
+      videoName={video.title}
+      videoDescription={video.description}
+    />
+  );
   return (
     <div className="videoGallery">
       <div className="videoGallery__header">
         <div className="videoGallery__heading">
           <Heading weight={2}>{props.heading}</Heading>
+          <FaYoutube
+          size={"20px"}
+          color={"#292D32"}
+          />
         </div>
         <div className="videoGallery__button">
           <Button
             label={"Add video"}
-            variant={"Primary"}
+            variant={"primary"}
             stretch={false}
+            onClick={props.buttonOnClick}
           />
         </div>
       </div>
-      <div className="videoGallery__ gallery"></div>
+      <div className="videoGallery__gallery">
+        {result}
+      </div>
     </div>
-  )
+  );
 }
+
+VideoGallery.PropTypes = {
+  content: PropTypes.arrayOf(
+    PropTypes.shape({
+      url: PropTypes.string,
+      title: PropTypes.string,
+      description: PropTypes.string
+    })),
+  heading: PropTypes.string,
+  buttonOnClick: PropTypes.func,
+};
+VideoGallery.defaultProps = {
+  content: undefined,
+  heading: undefined,
+  buttonOnClick: undefined
+};
