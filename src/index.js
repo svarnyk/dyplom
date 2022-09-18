ece80683b752b667e0a6f469bb6f1dc90d85a305
svarnyk <svarnyk@gmail.com>
2022-09-18 11:24:46 +0300

    fix errors from git

--- src/index.js
+++ src/index.js
@@ -14,14 +14,10 @@ app.render(
     {/*<StyleLoader />*/}
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-      <PersistGate loading={null} persistor={persistor}>
       <App/>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
--- src/store/index.js
+++ src/store/index.js
@@ -51,27 +51,3 @@ const store = configureStore({
 export const persistor = persistStore(store)
 
 export default store
-
-
-const persistConfig = {
-  key: "root",
-  storage: storage,
-  whitelist:["authUser"]
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      }
-    })
-})
-
-export const persistor = persistStore(store)
-
-export default store
-
